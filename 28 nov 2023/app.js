fa074d0d3661c7fdea600336dd688a0b1add9503
vinupflairs <145760856+vinupflairs@users.noreply.github.com>
2023-11-28 14:18:46 +0530

    app.js

--- 28 nov 2023/app.js
+++ 28 nov 2023/app.js
@@ -5,39 +5,42 @@ const { MongoClient } = require('mongodb');
 const app = express();
 const port = 3000;
 
+const url = 'mongodb://localhost:27017'; 
+const dbName = 'mydatabase';
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
-
-const uri = 'mongodb://localhost:27017/formsubmit';
-const client = new MongoClient(uri);  
-
-client.connect(err => {
-    if (err) {
-        console.error('Failed to connect to the database');
-    } else {
-        console.log('Connected to the database');
-    }
-});
 
 app.get('/register', (req, res) => {
     res.render('register');
 });
 
-app.post('/register', (req, res) => {
-    const { name, email } = req.body;
+app.post('/register', async (req, res) => {
+    const { name, email, mobile } = req.body;
 
-    const collection = client.db().collection('users');
-    collection.insertOne({ name, email }, (err) => {
-        if (err) {
-            console.error('Error inserting data into MongoDB', err);
-            res.status(500).send('Internal Server Error');
-        } else {
-            console.log(`Name: ${name}, Email: ${email} saved to MongoDB`);
-            res.send('Thank you for submitting the form. Your data has been registered successfully!');
-        }
-    });
+   
+    const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    try {
+        await client.connect();
+        console.log('Connected to the database');
+
+       
+        const db = client.db(dbName);
+
+       
+        const usersCollection = db.collection('users');
+        await usersCollection.insertOne({ name, email, mobile });
+
+        console.log(`Name: ${name}, Email: ${email}, Mobile: ${mobile}`);
+        res.send('Thank you for registering!');
+    } finally {
+      
+        await client.close();
+        console.log('Connection closed');
+    }
 });
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
